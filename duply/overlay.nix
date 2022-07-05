33636c193fd76ebf946c6bc5aab6ed3e008da996
Markus John <johm@quyo.de>
2022-07-05 20:09:04 +0200

    Minor improvements

--- duply/overlay.nix
+++ duply/overlay.nix
@@ -2,7 +2,7 @@ self: super:
 rec {
 
   duplicity = super.duplicity.overrideAttrs (oldAttrs: {
-    pythonPath = oldAttrs.pythonPath ++ [ super.python3.pkgs.boto ];
+    pythonPath = (oldAttrs.pythonPath or []) ++ [ super.python3.pkgs.boto ];
   });
 
   duply = super.duply.override { inherit duplicity; };
