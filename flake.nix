249a2d015a46be019f2f60e9b29a107d9ec1c6a1
Markus John <johm@quyo.de>
2022-07-05 18:59:48 +0200

    Add mersenneforumorg as external flake

--- flake.nix
+++ flake.nix
@@ -3,14 +3,19 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/release-22.05";
     flake-utils.url = "github:numtide/flake-utils";
+    mersenneforumorg.url = "github:Samayel/mersenneforumorg.nix";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, mersenneforumorg }:
     flake-utils.lib.simpleFlake {
       inherit self nixpkgs;
       name = "qnixpkgs-flake";
       config = { };
-      overlay = ./overlay.nix;
+      overlay = import ./overlay.nix (
+        mersenneforumorg.legacyPackages.x86_64-linux //
+        { }
+      );
+      systems = [ "x86_64-linux" ];
     };
 
 }
--- overlay.nix
+++ overlay.nix
@@ -1,7 +1,14 @@
-self: super:
+externals: self: super:
+
+let
+
+  duply = import duply/overlay.nix self super;
+
+in
+
 {
 
   # this key should be the same as the flake name attribute.
-  qnixpkgs-flake = import duply/overlay.nix self super;
+  qnixpkgs-flake = externals // duply;
 
 }
