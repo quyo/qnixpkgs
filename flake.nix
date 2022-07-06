03346a2dbf99de2c6f191e39d0d96f7e1cc75949
Markus John <johm@quyo.de>
2022-07-06 21:35:29 +0200

    Add shellscripts as external flake

--- flake.nix
+++ flake.nix
@@ -3,19 +3,26 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/release-22.05";
     flake-utils.url = "github:numtide/flake-utils";
+
+    shellscripts.url = "github:Samayel/shellscripts.nix";
+    shellscripts.inputs.nixpkgs.follows = "nixpkgs";
+
     mersenneforumorg.url = "github:Samayel/mersenneforumorg.nix";
+    mersenneforumorg.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, flake-utils, mersenneforumorg }:
+  outputs = { self, nixpkgs, flake-utils, shellscripts, mersenneforumorg }:
     flake-utils.lib.simpleFlake {
       inherit self nixpkgs;
       name = "qnixpkgs-flake";
       config = { };
       overlay = import ./overlay.nix (
-        mersenneforumorg.legacyPackages.x86_64-linux //
-        { }
+        shellscripts.legacyPackages.x86_64-linux //
+        mersenneforumorg.legacyPackages.x86_64-linux
       );
       systems = [ "x86_64-linux" ];
+    } // {
+      packages.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.hello;
     };
 
 }
