3ae35f0d82c7406662fd9f2f5c15e53c5ca43040
Markus John <johm@quyo.de>
2022-07-15 19:56:29 +0200

    Extend cas

--- cas/overlay.nix
+++ cas/overlay.nix
@@ -1,5 +1,9 @@
 final: prev:
 
+let
+  py3 = prev.python3.withPackages (p: with p; [ gmpy2 mpmath ]);
+in
+
 {
 
   cas = prev.symlinkJoin
@@ -7,9 +11,12 @@ final: prev:
     name = "cas";
     preferLocalBuild = false;
     allowSubstitutes = true;
+
     paths = with prev; [
       maxima
       octave
+      pari
+      py3
 #     sagetex
 #     sageWithDoc
       yacas
