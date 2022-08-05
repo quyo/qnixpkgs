f711d20cc8fe92d85f19c8fd93e04de13b1a33eb
Markus John <johm@quyo.de>
2022-08-05 15:32:22 +0200

    Add bat-extras from nixpkgs

---
+++ bat-extras/overlay.nix
@@ -0,0 +1,12 @@
+self: final: prev:
+let
+  bat-extras = final.recurseIntoAttrs (final.callPackages ./. { });
+in
+{
+  batdiff = bat-extras.batdiff;
+  batgrep = bat-extras.batgrep;
+  batman = bat-extras.batman;
+  # batpipe = bat-extras.batpipe;
+  batwatch = bat-extras.batwatch;
+  prettybat = bat-extras.prettybat;
+}
