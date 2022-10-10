b0b1d9083fe8a77b0e890a05521dd09aefb7283c
Markus John <johm@quyo.de>
2022-10-10 18:48:34 +0200

    Improve userprofile-local/flake.nix

--- dotfiles/src/.config/nix/userprofile-local/flake.nix
+++ dotfiles/src/.config/nix/userprofile-local/flake.nix
@@ -1,52 +1,42 @@
 {
 
   inputs = {
-    # nixpkgs-stable.url = "github:nixos/nixpkgs/release-22.05";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/a28adc36c20fd2fbaeb06ec9bbd79b6bf7443979";
-    # nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/0e304ff0d9db453a4b230e9386418fd974d5804a";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/release-22.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
     flake-utils.url = "github:numtide/flake-utils";
 
     qnixpkgs.url = "github:Samayel/qnixpkgs";
-    qnixpkgs.inputs.nixpkgs-stable.follows = "nixpkgs-stable";
-    qnixpkgs.inputs.nixpkgs-unstable.follows = "nixpkgs-unstable";
-    qnixpkgs.inputs.flake-utils.follows = "flake-utils";
     qnixpkgs.inputs.qnixpkgs.follows = "qnixpkgs";
   };
 
   outputs = { self, nixpkgs-stable, nixpkgs-unstable, flake-utils, qnixpkgs, ... }:
     let
-      version =
-        let
-          inherit (builtins) substring;
-          inherit (self) lastModifiedDate;
-        in
-        "0.${substring 0 8 lastModifiedDate}.${substring 8 6 lastModifiedDate}.${self.shortRev or "dirty"}";
+      inherit (builtins) attrNames concatMap getAttr substring;
+      inherit (nixpkgs-stable.lib) attrByPath importJSON;
+      inherit (self) lastModifiedDate;
 
-      json = nixpkgs-stable.lib.importJSON ./packages.json;
+      version = "0.${substring 0 8 lastModifiedDate}.${substring 8 6 lastModifiedDate}.${self.shortRev or "dirty"}";
+
+      json = importJSON ./packages.json;
 
       getFlakePkgs = system: label:
         let
-          inherit (builtins) getAttr;
-
           outs = (getAttr label self).outputs;
-          pkgs = (outs.packages or outs.legacyPackages).${system};
+          pkgs = (attrByPath [ "legacyPackages" system ] { } outs) // (attrByPath [ "packages" system ] { } outs);
         in
         map
           (x: getAttr x pkgs)
           (getAttr label json);
 
-      getAllPkgs = system:
-        let inherit (builtins) attrNames concatMap;
-        in concatMap (getFlakePkgs system) (attrNames json);
+      getAllPkgs = system: concatMap (getFlakePkgs system) (attrNames json);
     in
     {
       # needed by (getAttr label self) in getFlakePkgs
       inherit nixpkgs-stable nixpkgs-unstable qnixpkgs;
     }
     //
-    flake-utils.lib.eachSystem [ flake-utils.lib.system.x86_64-linux flake-utils.lib.system.armv7l-linux ] (system:
+    flake-utils.lib.eachSystem (map (x: flake-utils.lib.system.${x}) [ "x86_64-linux" "armv7l-linux" ]) (system:
       let
         inherit (nixpkgs-stable.outputs.legacyPackages.${system}) buildEnv;
       in
