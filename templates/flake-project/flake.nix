5fa8308ff138004ba06fb190a4e43f1900fa4e2c
Markus John <johm@quyo.de>
2022-10-31 18:17:16 +0100

    Improve flake-project template

--- templates/flake-project/flake.nix
+++ templates/flake-project/flake.nix
@@ -11,71 +11,43 @@
   };
 
   outputs = { self, nixpkgs-stable, nixpkgs-unstable, flake-utils, devshell, ... }:
-    let
-      inherit (builtins) attrNames concatMap getAttr substring;
-      inherit (nixpkgs-stable.lib) attrByPath importJSON;
-      inherit (self) lastModifiedDate;
-
-      version = "0.${substring 0 8 lastModifiedDate}.${substring 8 6 lastModifiedDate}.${self.shortRev or "dirty"}";
-
-      json = importJSON ./devenv-packages.json;
-
-      getFlakePkgs = system: label:
-        let
-          outs = (getAttr label self).outputs;
-          pkgs = (attrByPath [ "legacyPackages" system ] { } outs) // (attrByPath [ "packages" system ] { } outs);
-        in
-        map
-          (x: getAttr x pkgs)
-          (getAttr label json);
-
-      getAllPkgs = system: concatMap (getFlakePkgs system) (attrNames json);
-    in
     {
       # needed by (getAttr label self) in getFlakePkgs
       inherit nixpkgs-stable nixpkgs-unstable self;
-
-      overlays = {
-        default = import ./overlay.nix self;
-      };
     }
     //
     flake-utils.lib.eachSystem (map (x: flake-utils.lib.system.${x}) [ "x86_64-linux" ]) (system:
       let
-        overlays = (builtins.attrValues self.overlays) ++ [
+        overlays = [
+          (import ./overlay.nix system self)
           devshell.overlay
         ];
 
-        pkgs-stable = import nixpkgs-stable { inherit overlays system; };
-        pkgs-unstable = import nixpkgs-unstable { inherit overlays system; };
+        pkgs = import nixpkgs-stable { inherit overlays system; };
       in
       {
         packages = rec {
-          my-project = pkgs-stable.my-project;
-          my-project-devenv = pkgs-stable.buildEnv
-            {
-              name = "my-project-devenv-${version}";
-              paths = getAllPkgs system;
-            };
+          my-project = pkgs.my-project;
+          my-project-devenv = pkgs.my-project-devenv;
           default = my-project;
         };
 
         apps = rec {
-          my-project = { type = "app"; program = "${pkgs-stable.my-project}/bin/hello"; };
+          my-project = { type = "app"; program = "${pkgs.my-project}/bin/hello"; };
           default = my-project;
         };
 
         devShells = {
           default =
             let
-              inherit (pkgs-stable.devshell) mkShell importTOML;
+              inherit (pkgs.devshell) mkShell importTOML;
             in
             mkShell {
               imports = [ (importTOML ./devshell.toml) ];
             };
         };
 
-        formatter = pkgs-stable.nixpkgs-fmt;
+        formatter = pkgs.nixpkgs-fmt;
       }
     );
 }
--- templates/flake-project/overlay.nix
+++ templates/flake-project/overlay.nix
@@ -1,4 +1,33 @@
-self: final: prev:
+system: self: final: prev:
+
+let
+  devenv =
+    let
+      inherit (builtins) attrNames concatMap getAttr;
+      inherit (final) buildEnv;
+      inherit (final.lib) attrByPath importJSON;
+
+      json = importJSON ./devenv-packages.json;
+
+      getFlakePkgs = system: label:
+        let
+          outs = (getAttr label self).outputs;
+          pkgs = (attrByPath [ "legacyPackages" system ] { } outs) // (attrByPath [ "packages" system ] { } outs);
+        in
+        map
+          (x: getAttr x pkgs)
+          (getAttr label json);
+
+      getAllPkgs = system: concatMap (getFlakePkgs system) (attrNames json);
+    in
+    buildEnv
+      {
+        name = "my-project-devenv";
+        paths = getAllPkgs system;
+      };
+in
+
 {
   my-project = final.hello;
+  my-project-devenv = devenv;
 }
