a5353d68ceca62fc3ad62c3732b124d866910046
Markus John <johm@quyo.de>
2022-11-06 17:14:47 +0100

    Improve node-express-project template (configuration management)

---
+++ templates/node-express-project/config/development.cjs
@@ -0,0 +1,7 @@
+const config = {
+  env: {
+    label: "DEV"
+  }
+};
+
+module.exports = config;
--- templates/node-express-project/src/backend/app.ts
+++ templates/node-express-project/src/backend/app.ts
@@ -1,10 +1,9 @@
-import express, { Application, Request, Response } from "express";
+import config from 'config';
+import express, { Application, Request, Response } from 'express';
 import expressLayouts from 'express-ejs-layouts';
-import http from "http";
-import path from "path";
+import http from 'http';
+import path from 'path';
 import url from 'url';
-
-import debug from "./config/debug";
 
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -16,7 +15,7 @@ const app: Application = express();
 app.use(expressLayouts);
 
 // Serve static / public files
-const publicDirectoryPath = path.join(__dirname, "../frontend/static");
+const publicDirectoryPath = path.join(__dirname, '../frontend/static');
 app.use('/static', express.static(publicDirectoryPath));
 
 // Setting the root path for views directory
@@ -26,11 +25,11 @@ app.set('views', path.join(__dirname, '../frontend/views'));
 app.set('view engine', 'ejs');
 
 /* Home route */
-app.get("/", (req: Request, res: Response) => {
-    res.render("index")
+app.get('/', (req: Request, res: Response) => {
+    res.render('index')
 });
 
-app.get("/:name(index|foo|bar)", (req: Request, res: Response) => {
+app.get('/:name(index|foo|bar)', (req: Request, res: Response) => {
     var name = req.params.name;
     res.render(name);
 });
@@ -39,9 +38,18 @@ app.get("/:name(index|foo|bar)", (req: Request, res: Response) => {
 const server: http.Server = http.createServer(app);
 
 // Setting the port
-const port = debug.PORT;
+const port = config.get('backend.server.port');
 
 // Starting the server
 server.listen(port, () => {
-    console.log(`SERVER RUNNING ON http://localhost:${port} (${process.env.NODE_ENV})`);
+    console.log('###########################################################################################################');
+    console.log('#')
+    console.log('#')
+    console.log(`#     SERVER RUNNING ON http://localhost:${port}`);
+    console.log('#')
+    console.log(`#         process.env.NODE_ENV = ${process.env.NODE_ENV}`);
+    console.log(`#         config.env.label = ${config.get('env.labelOutput')}`);
+    console.log('#')
+    console.log('#')
+    console.log('###########################################################################################################');
 });
