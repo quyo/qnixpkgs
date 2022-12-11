82bd74821801ac258919176b8ac194449f7e6377
Markus John <johm@quyo.de>
2022-12-11 19:40:05 +0100

    Improve node-nest-project template

--- templates/node-nest-project/postcss.config.cjs
+++ templates/node-nest-project/postcss.config.cjs
@@ -1,9 +1,11 @@
+const autoprefixer = require("autoprefixer");
 const postcssImport = require("postcss-import");
 const postcssPresetEnv = require("postcss-preset-env");
 
 module.exports = {
   plugins: [
     postcssImport(),
+    /* autoprefixer(), */
     postcssPresetEnv({
       /* use stage 2 features + css nesting rules */
       stage: 2,
--- templates/node-nest-project/src/frontend/public/js/app.ts
+++ templates/node-nest-project/src/frontend/public/js/app.ts
@@ -1,3 +1,23 @@
+// Bootstrap demo
+
+import { Popover } from "bootstrap";
+
+new Popover(document.getElementById("popoverButton") ?? "");
+
+// jQuery demo
+
+import jQuery from "jquery";
+/* eslint-disable */
+(<any>window).$ = jQuery;
+(<any>window).jQuery = jQuery;
+/* eslint-enable */
+
+jQuery(function () {
+  $("p").text("The DOM is now loaded and can be manipulated.");
+});
+
+// Typescript demo
+
 import Professor from "./professor";
 import Student from "./student";
 
@@ -6,4 +26,7 @@ const professor: Professor = new Professor("James Mathew");
 const student: Student = new Student("Rohit Lakhotia", professor);
 
 console.log(student.getFavProfessor());
+
+// NODE_ENV demo
+
 console.log(process.env["NODE_ENV"]);
