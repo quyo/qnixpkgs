22a5746074cb9dfb905720f601f849f8966e501f
Markus John <johm@quyo.de>
2025-07-27 10:11:10 +0000

    Update checkexec cargoHash

--- checkexec/default.nix
+++ checkexec/default.nix
@@ -11,5 +11,5 @@ rustPlatform.buildRustPackage rec {
     sha256 = "sha256-osLtyVXR4rASwRJmbu6jD8o3h12l/Ty4O8/XTl5UzB4=";
   };
 
-  cargoHash = "sha256-ivNhvd+Diq54tmJfveJoW8F/YN294/zRCbsQPwpufak=";
+  cargoHash = "sha256-TqmcIyokNM4SnopnzzfM6yyZQz79BIxTY38XjGsnj+s=";
 }
