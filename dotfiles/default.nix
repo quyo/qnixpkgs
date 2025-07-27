16c4791a0c30d33253fff885f6261154b1688072
Markus John <johm@quyo.de>
2025-07-27 12:29:48 +0000

    Adapt dotfiles to NixOS

--- dotfiles/default.nix
+++ dotfiles/default.nix
@@ -12,6 +12,7 @@ stdenvNoCC.mkDerivation {
     runHook preBuild
 
     mkdir -p $out/etc/dotfiles
+    mkdir -p $out/share/dotfiles
     cd src
 
     find . -mindepth 1 -maxdepth 1 ! -iname "*.tmpl" | xargs -i cp -r {} $out/etc/dotfiles
@@ -20,6 +21,15 @@ stdenvNoCC.mkDerivation {
     for dir in ''${dirs[@]}; do
       for file in $dir/* ; do
         cat $file >> $out/etc/dotfiles/''${dir/%.tmpl/}
+      done
+    done
+
+    find . -mindepth 1 -maxdepth 1 ! -iname "*.tmpl" | xargs -i cp -r {} $out/share/dotfiles
+
+    dirs=($(find . -mindepth 1 -maxdepth 1 -type d -iname "*.tmpl"))
+    for dir in ''${dirs[@]}; do
+      for file in $dir/* ; do
+        cat $file >> $out/share/dotfiles/''${dir/%.tmpl/}
       done
     done
 
