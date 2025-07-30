f3407c0688adabd34f0ba5720ccd871749dd5ff7
Markus John <johm@quyo.de>
2025-07-30 11:11:32 +0000

    Update NixOS configuration

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -5,10 +5,12 @@
     extra-substituters = [
       "https://nix-community.cachix.org"
       "https://quyo-public.cachix.org"
+      "ssh://eu.nixbuild.net?priority=90"
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "quyo-public.cachix.org-1:W83ifK7/6EvKU4Q2ZxvHRAkiIRzPeXYnp9LWHezs5U0="
+      "nixbuild.net/quyo-1:TaAsUc6SBQnXhUQJM4s+1oQlTKa1e3M0u3Zqb36fbRc="
     ];
   };
 
--- nixos/qos-nixos.nix
+++ nixos/qos-nixos.nix
@@ -1,16 +1,33 @@
 { config, lib, pkgs, ... }:
 
 {
+  nix.settings.extra-substituters = [
+    "https://nix-community.cachix.org"
+    "https://quyo-public.cachix.org"
+    "ssh://eu.nixbuild.net?priority=90"
+  ];
+
+  nix.settings.extra-trusted-public-keys = [
+    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    "quyo-public.cachix.org-1:W83ifK7/6EvKU4Q2ZxvHRAkiIRzPeXYnp9LWHezs5U0="
+    "nixbuild.net/quyo-1:TaAsUc6SBQnXhUQJM4s+1oQlTKa1e3M0u3Zqb36fbRc="
+  ];
+
+  nix.settings = {
+    auto-optimise-store = true;
+    builders-use-substitutes = true;
+    experimental-features = [ "nix-command" "flakes" ];
+    require-sigs = true;
+    sandbox = "relaxed";
+    trusted-users = [ "root" "johm" ];
+  };
+
+  nixpkgs.config.allowUnfree = true;
 
   # Does not remove garbage collector roots, such as old system configurations.
   # The following command deletes old roots, removing the ability to roll back to them: nix-collect-garbage --delete-older-than 7d
   nix.gc.automatic = true;
   nix.gc.dates = "02:15";
-
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  nix.settings.trusted-users = [ "root" "johm" ];
-
-  nixpkgs.config.allowUnfree = true;
 
   # Use the system nixpkgs for nix commands
   # https://www.zknotes.com/page/use%20the%20system%20nixpkgs%20for%20nix%20commands
