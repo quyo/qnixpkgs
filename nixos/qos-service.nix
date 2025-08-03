4681f086be137f1f2862679408c88f94086a2190
Markus John <johm@quyo.de>
2025-08-03 07:27:54 +0000

    Update NixOS configuration

--- nixos/qos-service.nix
+++ nixos/qos-service.nix
@@ -4,33 +4,4 @@
 
   virtualisation.docker.enable = true;
 
-  systemd.timers.nixosupd = {
-    wantedBy = [ "timers.target" ];
-    timerConfig = {
-      OnCalendar = "daily";
-      Persistent = true;
-    };
-  };
-  systemd.services.nixosupd = {
-    script = ''
-      set -euo pipefail
-      IFS=$'\n\t'
-
-      PATH=$PATH:${pkgs.nix}/bin:${pkgs.nixos-rebuild}/bin:${pkgs.git}/bin
-
-      nix flake update nixpkgs           --flake /etc/nixos
-      nix flake update nixpkgs-unstable  --flake /etc/nixos
-      nixos-rebuild switch               --flake /etc/nixos
-
-      GIT_AUTHOR_NAME="NixOSupd service"  GIT_AUTHOR_EMAIL="nixosupd@${config.quyo.host.fqdn}" \
-      GIT_COMMITTER_NAME=$GIT_AUTHOR_NAME GIT_COMMITTER_EMAIL=$GIT_AUTHOR_EMAIL                \
-      git -C /etc/nixos commit -a -m "v$(date +%Y-%m-%d)-00"
-
-      nix-collect-garbage --delete-older-than 90d
-    '';
-    serviceConfig = {
-      Type = "oneshot";
-    };
-  };
-
 }
