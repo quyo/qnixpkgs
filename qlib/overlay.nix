ef8481736f275b4ba6caad4f10815fcae3a63b68
Markus John <johm@quyo.de>
2025-08-06 06:50:58 +0000

    Fix qlib.scanPaths

--- qlib/overlay.nix
+++ qlib/overlay.nix
@@ -90,12 +90,12 @@ let
     scanPaths = path:
       builtins.map (f: (path + "/${f}")) (
         builtins.attrNames (
-          lib.attrsets.filterAttrs (
+          final.lib.attrsets.filterAttrs (
             path: _type:
             (_type == "directory") # include directories
             || (
               (path != "default.nix") # ignore default.nix
-              && (lib.strings.hasSuffix ".nix" path) # include .nix files
+              && (final.lib.strings.hasSuffix ".nix" path) # include .nix files
             )
           ) (builtins.readDir path)
         )
