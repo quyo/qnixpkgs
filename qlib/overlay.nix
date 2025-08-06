c3d2682c60e2dc6d0d7774f10aa8b1767c3725bc
Markus John <johm@quyo.de>
2025-08-06 06:37:45 +0000

    Add qlib.scanPaths from ryan4yin/nix-config

--- qlib/overlay.nix
+++ qlib/overlay.nix
@@ -87,6 +87,20 @@ let
 
     removeOverrideFuncs = set: removeAttrs set [ "override" "overrideDerivation" ];
 
+    scanPaths = path:
+      builtins.map (f: (path + "/${f}")) (
+        builtins.attrNames (
+          lib.attrsets.filterAttrs (
+            path: _type:
+            (_type == "directory") # include directories
+            || (
+              (path != "default.nix") # ignore default.nix
+              && (lib.strings.hasSuffix ".nix" path) # include .nix files
+            )
+          ) (builtins.readDir path)
+        )
+      );
+
   };
 in
 
