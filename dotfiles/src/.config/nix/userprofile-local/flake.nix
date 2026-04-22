c40daa169aac957bd1ab4dca5d77568597363d04
Markus John <johm@quyo.de>
2026-04-22 11:02:59 +0000

    Upgrade userprofile-local to 25.11

--- dotfiles/src/.config/nix/userprofile-local/flake.nix
+++ dotfiles/src/.config/nix/userprofile-local/flake.nix
@@ -4,7 +4,7 @@
 {
 
   inputs = {
-    nixpkgs-stable.url = "github:nixos/nixpkgs/release-25.05";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/release-25.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
     flake-utils.url = "github:numtide/flake-utils";
